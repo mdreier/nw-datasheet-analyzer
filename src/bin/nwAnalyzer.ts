8a0bb80af720e75a44bbc6dd545b53200d9d9b98
Martin Dreier <martin@martindreier.de>
2021-11-15 19:34:46 +0100

    Convert analyzer into executable

---
+++ src/bin/nwAnalyzer.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import { mkdirSync, writeFileSync } from "fs";
+import commandLineArgs from 'command-line-args';
+import { CommandLine, commandLineDefinition, printHelp } from "../commandline.js";
+import { Analyzer, DataLoader, Formatter } from "../index.js";
+
+const commandLine = commandLineArgs(commandLineDefinition) as CommandLine;
+
+if (commandLine.help) {
+    printHelp();
+    process.exit();
+}
+
+let loader = new DataLoader();
+if (commandLine.forceDownload || !loader.dataFilesExist()) {
+    await loader.download();
+}
+
+let dataTables = loader.parse();
+let analyzer = new Analyzer(dataTables);
+let analyzedTables = analyzer.analyze();
+
+mkdirSync('./docs', {recursive: true});
+writeFileSync('./docs/lootTables.html', new Formatter(analyzedTables).html());
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Analyzer } from "./Analyzer";
+import { DataLoader } from "./DataLoader";
+import { Formatter } from "./Formatter";
+
+export {
+    DataLoader,
+    Formatter,
+    Analyzer
+}
