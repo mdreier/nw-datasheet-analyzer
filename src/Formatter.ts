b4ddc5534fe403f8d85df0c65d9248b522b85cbf
Martin Dreier <martin@martindreier.de>
2021-11-15 19:26:00 +0100

    Better formatting of loot table items

--- src/Formatter.ts
+++ src/Formatter.ts
@@ -1,10 +1,28 @@
-import { AnalyzedLootTable } from "./Loot";
+import { AnalyzedLootItem, AnalyzedLootTable, NumberRange } from "./Loot";
 import Handlebars from "handlebars";
 import { readFileSync } from "fs";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 const TEMPLATE_DIR = dirname(fileURLToPath(import.meta.url)) + "/templates/";
+
+Handlebars.registerHelper("range", function(range: NumberRange): string {
+    if (!range) {
+        return "";
+    } else if (range.Low === range.High) {
+        return "" + range.Low;
+    } else {
+        return range.Low + "-" + range.High;
+    }
+});
+
+Handlebars.registerHelper("probability", function(probability: number): string {
+    if (!probability) {
+        return "0.000%";
+    } else {
+        return (probability * 100).toFixed(3) + "%";
+    }
+});
 
 const Templates = {
     html: Handlebars.compile(readFileSync(TEMPLATE_DIR + "analysis.html", {encoding: 'utf-8'}))
